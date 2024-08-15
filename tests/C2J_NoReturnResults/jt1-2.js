6f8a2d4a77682efd4d2e5f3af2985511dd95dce8
Mahesh Maheswaran <maheswar@cs.mcgill.ca>
2024-08-15 18:56:08 -0400

    old jamterm deleted

---
+++ tests/C2J_NoReturnResults/jt1-2.js
@@ -0,0 +1,16 @@
+japp nspc {
+    required_linker_flags:-lm, -lc;
+    required_clibs: float.h, wchar.h, bazinga.h
+}
+
+let count = 0;
+
+jasync you(str: char*, num: int[]) {
+    count++;
+//    console.log("Message received: ", str, num, " local count ", count);
+}
+
+setInterval(()=> {
+    console.log("Count ", count);
+    count = 0;
+}, 1000);
