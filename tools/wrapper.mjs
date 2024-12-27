fc25e2d0f3fc55a29d30b3925e98577b0f048526
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2024-12-26 19:30:40 -0500

    clean

--- tools/wrapper.mjs
+++ tools/wrapper.mjs
@@ -6,18 +6,53 @@ import { spawnSync } from 'child_process';
 const { exec } = require('child_process');
 
 
-let endpoint = process.argv;
+import {
+    body_1,
+    header_1,
+    header_2,
+    body_sec,
+    body_2,
+    body_sec_warning,
+    body_2_bold,
+} from "./chalk.mjs";
+let args;
 
-if (endpoint[3] == "jamrun"){
-    let command = getJamrunArgsWrapper(endpoint.slice(4));
+function show_usage() {
+    const usageMessage = `
+    ${header_1(`JAMTools 2.0`)}
+
+    ${header_2(`wrapper`)}${body_1(
+        ` --  a tool to run curl commands in a friendly way`
+    )}
+
+    ${header_1(`SYNOPSIS`)}
+
+    Usage: wrapper 
+
+
+    --- wrapper.mjs endpoint [arguments to pass]
+
+    `;
+    
+    console.log(usageMessage);
+}
+
+//capture the endpoints arguments
+let endpoint_args = process.argv;
+
+// for jamrun, no need to provide the file name, jt1.jxe is used.
+// ex: zx wrapper.mjs jamrun --app="DEMO"
+if (endpoint_args[3] == "jamrun"){
+    // here we will work with the arguments
+    let command = getJamrunArgsWrapper(endpoint_args.slice(4));
     let curlCommand = `curl -X POST http://url:3000/jamrun -H "Content-Type: application/json" -d ${command}`;
 
     let cwd = '/root/capstone/JAMScript/tools/';
     const childProcess = exec(curlCommand, { cwd }); 
 }
 
-if (endpoint[3] == "jambatch.mjs"){
-    let command = getBatchArgsWrapper(endpoint.slice(4));
+if (endpoint_args[3] == "jambatch.mjs"){
+    let command = getBatchArgsWrapper(endpoint_args.slice(4));
     let curlCommand = `curl -X POST http://url:3000/jambatch -H "Content-Type: application/json" -d ${command}`;
 
     let cwd = '/root/capstone/JAMScript/tools/';
@@ -25,24 +60,24 @@ if (endpoint[3] == "jambatch.mjs"){
 
 }
 
-if (endpoint[3] == "jamlog"){
-    let command = getLogArgsWrapper(endpoint.slice(4));
+if (endpoint_args[3] == "jamlog"){
+    let command = getLogArgsWrapper(endpoint_args.slice(4));
     let curlCommand = `curl -X POST http://url:3000/jamlog -H "Content-Type: application/json" -d ${command}`;
     
     let cwd = '/root/capstone/JAMScript/tools/';
     const childProcess = exec(curlCommand, { cwd });
 }
 
-if (endpoint[3] == "jamlist"){
-    let command = getJamListArgsWrapper(endpoint.slice(4));
+if (endpoint_args[3] == "jamlist"){
+    let command = getJamListArgsWrapper(endpoint_args.slice(4));
     let curlCommand = `curl -X POST http://url:3000/jamlist -H "Content-Type: application/json" -d ${command}`;
 
     let cwd = '/root/capstone/JAMScript/tools/';
     const childProcess = exec(curlCommand, { cwd });
 }
 
-if (endpoint[3] == "jamkill"){
-    let command = getJamKillArgsWrapper(endpoint.slice(4));
+if (endpoint_args[3] == "jamkill"){
+    let command = getJamKillArgsWrapper(endpoint_args.slice(4));
     let curlCommand = `curl -X POST http://url:3000/jamkill -H "Content-Type: application/json" -d ${command}`;
 
     let cwd = '/root/capstone/JAMScript/tools/';
@@ -50,8 +85,8 @@ if (endpoint[3] == "jamkill"){
 
 }
 
-if (endpoint[3] == "jamterm"){
-    let command = getJamTermArgsWrapper(endpoint.slice(4));
+if (endpoint_args[3] == "jamterm"){
+    let command = getJamTermArgsWrapper(endpoint_args.slice(4));
     let curlCommand = `curl -X POST http://url:3000/jamterm -H "Content-Type: application/json" -d ${command}`;
 
     let cwd = '/root/capstone/JAMScript/tools/';
