af97b299c5389ab099fc3be3afd176d4a5b73735
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2025-01-15 23:35:12 -0500

    done app-docker

--- tools/app-docker.js
+++ tools/app-docker.js
@@ -5,6 +5,24 @@ const app = express();
 const port = 3000;
 const host = '0.0.0.0';
 
+
+/// TO UPDATE IN FUNCTION OF WHERE THE TOOLS ARE ON YOUR LOCAL COPY ///
+let cwd = '/Users/andreisandor/Desktop/McGill/Capstone/newServer/JAMScript/tools';
+const childProcessExport1 = exec('export JAM_HOME=/Users/andreisandor/Desktop/McGill/Capstone/newServer/JAMScript',  
+                                  {cwd} );
+
+const childProcessExport2 = exec('export JAMHOME=/Users/andreisandor/Desktop/McGill/Capstone/newServer/JAMScript',  
+                                  {cwd});
+const childProcessExport3 = exec('export PATH=$JAM_HOME/tools:$PATH',  
+                                  {cwd});
+const childProcessExport4 = exec('export JAMDATA=/Users/andreisandor/Desktop/McGill/Capstone/newServer/JAMScript/data', {cwd});
+
+
+const childProcess1 = exec('djam init --ideal',  {cwd} );
+const childProcess2 = exec('djam pull mahes25/jamscript',  {cwd});
+////////////////////////////////////////////////////////////////////////////
+
+
 // Middleware to parse JSON requests
 app.use(express.json());
 
@@ -77,7 +95,7 @@ app.post('/jamrun', (req, res) => {
     return res.status(400).json({ error: 'The "app_name" field is required.' });
   }
 
-  let command = `zx jamrun.mjs ${file} --app=${app_name}`;
+  let command = `djam run ${file} --app=${app_name}`;
   if (fog) command += ' --fog';
   if (cloud) command += ' --cloud';
   if (device) command += ' --device';
@@ -98,35 +116,35 @@ app.post('/jamrun', (req, res) => {
 });
 
 // Define the /jambatch endpoint
-app.post('/jambatch', (req, res) => {
-  const {
-    fog, device, cloud, cFile, fFile, dFile, num, cLoc, fLoc, dLoc, cEdge, fEdge, dEdge
-  } = req.body;
+// app.post('/jambatch', (req, res) => {
+//   const {
+//     fog, device, cloud, cFile, fFile, dFile, num, cLoc, fLoc, dLoc, cEdge, fEdge, dEdge
+//   } = req.body;
 
-  let command = `zx jambatch.mjs`;
-  if (fog) command += ` --fog=${fog}`;
-  if (device) command += ` --device=${device}`;
-  if (cloud) command += ` --cloud=${cloud}`;
-  if (cFile) command += ` --cFile=${cFile}`;
-  if (fFile) command += ` --fFile=${fFile}`;
-  if (dFile) command += ` --dFile=${dFile}`;
-  if (num) command += ` --num=${num}`;
-  if (cLoc) command += ` --cLoc=${cLoc}`;
-  if (fLoc) command += ` --fLoc=${fLoc}`;
-  if (dLoc) command += ` --dLoc=${dLoc}`;
-  if (cEdge) command += ` --cEdge=${cEdge}`;
-  if (fEdge) command += ` --fEdge=${fEdge}`;
-  if (dEdge) command += ` --dEdge=${dEdge}`;
+//   let command = `zx jambatch.mjs`;
+//   if (fog) command += ` --fog=${fog}`;
+//   if (device) command += ` --device=${device}`;
+//   if (cloud) command += ` --cloud=${cloud}`;
+//   if (cFile) command += ` --cFile=${cFile}`;
+//   if (fFile) command += ` --fFile=${fFile}`;
+//   if (dFile) command += ` --dFile=${dFile}`;
+//   if (num) command += ` --num=${num}`;
+//   if (cLoc) command += ` --cLoc=${cLoc}`;
+//   if (fLoc) command += ` --fLoc=${fLoc}`;
+//   if (dLoc) command += ` --dLoc=${dLoc}`;
+//   if (cEdge) command += ` --cEdge=${cEdge}`;
+//   if (fEdge) command += ` --fEdge=${fEdge}`;
+//   if (dEdge) command += ` --dEdge=${dEdge}`;
 
-  executeCommand(req, res, command);
-});
+//   executeCommand(req, res, command);
+// });
 
 // Define the /jamlog endpoint
 app.post('/jamlog', (req, res) => {
   const { help, program, app, port, remote, tail, c, j } = req.body;
 
   // Default command without arguments if none are specified
-  let command = `zx jamlog.mjs`;
+  let command = `djam log`;
   if (help) command += ` --help`;
   if (program) command += ` --program=${program}`;
   if (app) command += ` --app=${app}`;
@@ -145,7 +163,7 @@ app.post('/jamlist', (req, res) => {
   const { help, all, monitor, type, dataStore, tmuxid, port, app, prog, remote } = req.body;
 
   // Default command without arguments if none are specified
-  let command = `zx jamlist.mjs`;
+  let command = `djam list`;
   if (help) command += ` --help`;
   if (all) command += ` --all`;
   if (monitor) command += ` --monitor`;
@@ -164,7 +182,7 @@ app.post('/jamlist', (req, res) => {
 app.post('/jamkill', (req, res) => {
   const { reset, all, remote, app, prog, port } = req.body;
 
-  let command = 'zx jamkill.mjs';
+  let command = 'djam kill';
   if (reset) command += ' --reset';
   if (all) command += ' --all';
   if (remote) command += ` --remote=${remote}`;
@@ -179,7 +197,7 @@ app.post('/jamkill', (req, res) => {
 app.post("/jamterm", (req, res) => {
   const { all, app, prog, port, pane } = req.body;
 
-  let command = `zx jamterm.mjs`;
+  let command = `djam term`;
   if (all) command += ' --all';
   if (app) command += ` --app==${app}`;
   if (prog) command += ` --prog==${prog}`;
